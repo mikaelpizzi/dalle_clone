2e932efd129e7a38f3075acba52d27ec62c85f14
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2023-01-21 21:26:12 -0400

    feat: Implement handlers

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,8 +15,13 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false);
   const generateImage = () => {};
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
